fba742817c030ecc3f575c8814cf8b9708667f2d
Felix Galvin <felixgalvin1709@gmail.com>
2024-10-21 10:58:09 +0700

    baru

--- src/Magister.java
+++ src/Magister.java
@@ -1,4 +1,8 @@
 public class Magister extends Mahasiswa{
     String mataKuliah;
     String judulTesis;
+
+    public Magister(String nama, String alamat, String TTL, String telepon) {
+        super(nama, alamat, TTL, telepon);
+    }
 }
